5ded26d6f5e9432b183596873b3303c3be5c5d34
zhan yan <1075465357lin@gmail.com>
2024-01-08 19:40:56 +0800

    init core model

---
+++ core/src/test/java/com/ader1y/init/AppTests.java
@@ -0,0 +1,13 @@
+package com.ader1y.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
