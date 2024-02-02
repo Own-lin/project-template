7b70952c7f7e02b9c50a06f8af9e8399d04eb398
zhan yan <1075465357lin@gmail.com>
2024-02-02 14:09:56 +0800

    优化API案例

--- core/src/main/java/com/ader1y/template/core/server/v1/ExampleServer.java
+++ core/src/main/java/com/ader1y/template/core/server/v1/ExampleServer.java
@@ -1,19 +1,17 @@
 package com.ader1y.template.core.server.v1;
 
+import com.ader1y.template.client.api.v1.ExampleClient;
 import com.ader1y.template.model.VO.ExampleInfoVO;
 import com.ader1y.template.model.base.R;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/example")
-public class ExampleServer {
+public class ExampleServer implements ExampleClient {
 
-    @GetMapping("/info")
-    public R<ExampleInfoVO> getInfo(){
-
+    @Override
+    public R<ExampleInfoVO> getInfo() {
         return R.success(new ExampleInfoVO("I'm example server.", "empty remark"));
     }
-
 }
